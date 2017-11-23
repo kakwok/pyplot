188db22e7954e6be6fc771608f998a416ace6cb3
Martin <ka_hei_martin_kwok@brown.edu>
2017-11-23 11:17:18 +0100

    Add example usage

--- plot_example.py
+++ plot_example.py
@@ -2,8 +2,9 @@ from ROOT import *
 from hPlus import *
 from style import *
 from util import *
-import glob
+import glob,tdrstyle
 
+tdrstyle.setTDRStyle()
 #global variables
 flistOfDict= []
 Pages      = []
@@ -19,6 +20,15 @@ Setting ={"xlow":0}
 for i in range(2,12):
     page  = getHlistFromFiles(flistOfDict,"ST/stExc%02iHist"%i,"ST/GeV","N")
     Pages.append({'Title':"ST(N=%s)"%i,"list":page,"legend":legend,"Setting":Setting})
+#Make a page from same file, different histograms
+histos = [
+    {'hLabel':"Jets in N=%i events, abs(#eta)>3.0, E_{T}>%s","hname":"Jets/Jet_ST_ISR_Exc03"},
+    {'hLabel':"Jets in N=%i events, dRmin <1.0, E_{T}>%s"   ,"hname":"Jets/Jet_ST_FSR_Exc03"},
+    {'hLabel':"Jets in N=%i events, E_{T}>%s"               ,"hname":"ST/stExc03Hist"       }
+]
+page  = getHlistFromNames(f['fname'],histos,"ST[GeV]","Fraction of entries")
+selfnormalize(page,2000,7000)
+Pages.append({'Title':"IsoJet,ST>2TeV,(N=%s)"%i,"list":page,"legend":legend,"Setting":Setting})
 
 c1 = TCanvas("c1","c1",800,600)
 gStyle.SetOptStat(0)
